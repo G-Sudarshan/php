49f840823ff02ba7f402affcfc6166ccd6deeba8
G Sudarshan <43257931+G-Sudarshan@users.noreply.github.com>
2020-02-19 17:39:53 +0530

    Add files via upload

---
+++ Tricky questions set 2/3.php
@@ -0,0 +1,18 @@
+<?php
+$var = 'PHP Tutorial';
+?>
+<!DOCTYPE html>
+<?php
+$var = 'Github account'
+
+?><html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <title><?php echo $var; ?> - My Github account!</title>
+  </head>
+  <body>
+  <h3><?php echo $var; ?></h3>
+  
+  <p><a href="https://github.com/G-Sudarshan">Go to the <?php echo $var; ?></a>.</p>
+</body>
+</html>
